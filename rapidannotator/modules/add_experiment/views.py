49e9e970570f56eda73d9d588e9b7e513f9d7013
gulshan-kumar <gulshan.kumar@research.iiit.ac.in>
2019-03-04 18:49:48 +0530

    Solve Issues 4,5,6, Now multiple images can be uploaded and renaming doesn't show any error

--- rapidannotator/modules/add_experiment/views.py
+++ rapidannotator/modules/add_experiment/views.py
@@ -267,9 +267,7 @@ def _editLabel():
 @blueprint.route('/_uploadFiles', methods=['POST','GET'])
 def _uploadFiles():
     from rapidannotator import app
-
     if request.method == 'POST':
-
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -311,6 +309,17 @@ def _uploadFiles():
 
 
                 db.session.commit()
+                if experiment.uploadType == 'manual' and experiment.category != 'text':
+                    currentFile = File.query.filter_by(id=newFile.id).first()
+                    file_name, file_extension = os.path.splitext(currentFile.name)
+                    updatedName = secure_filename(file_name + '_' + str(currentFile.id) + file_extension)
+                    print(updatedName)
+                    experimentDir = os.path.join(app.config['UPLOAD_FOLDER'],
+                                    str(currentFile.experiment_id))
+                    currentFilePath = os.path.join(experimentDir, currentFile.name)
+                    newFilePath = os.path.join(experimentDir, updatedName)
+                    os.rename(currentFilePath, newFilePath)
+                    db.session.commit()
 
                 response = {
                     'success' : True,
@@ -386,8 +395,8 @@ def _deleteFile():
                 'error' : "specified experiment doesn't have any file",
             }
             return jsonify(response)
-
-        filePath = os.path.join(experimentDir, currFile.content)
+        currfile_name, currfile_extension = os.path.splitext(currFile.content)
+        filePath = os.path.join(experimentDir, currfile_name + '_' + str(currFile.id) + currfile_extension)
         os.remove(filePath)
 
     db.session.delete(currFile)
@@ -404,16 +413,19 @@ def _updateFileName():
     fileId = request.args.get('fileId', None)
     currentFile = File.query.filter_by(id=fileId).first()
     experiment = Experiment.query.filter_by(id=currentFile.experiment_id).first()
-
     updatedName = secure_filename(request.args.get('name', currentFile.name))
+    currentfile_name, currentfile_extension = os.path.splitext(currentFile.name)
+    updatedfile_name, updatedfile_extension = os.path.splitext(updatedName)
 
     if experiment.uploadType == 'manual' and experiment.category != 'text':
         experimentDir = os.path.join(app.config['UPLOAD_FOLDER'],
                                     str(currentFile.experiment_id))
 
-        currentFilePath = os.path.join(experimentDir, currentFile.name)
-        newFilePath = os.path.join(experimentDir, updatedName)
+        currentFilePath = os.path.join(experimentDir, currentfile_name + '_' + str(currentFile.id) + currentfile_extension)
+        updated_name_id = secure_filename(updatedfile_name + '_' + str(currentFile.id) + updatedfile_extension)
+        newFilePath = os.path.join(experimentDir, updated_name_id)
         os.rename(currentFilePath, newFilePath)
+        currentFile.content = updatedName
 
     currentFile.name = updatedName
 
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/rapidannotator/')
-
-from rapidannotator import app as application
